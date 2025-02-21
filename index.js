7f4cbb168de63631e6ad25785d9450e31900eeea
developer-ajay <developer.ajay618@gmail.com>
2025-02-21 17:40:33 +0530

    dynamic city from user input

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const apiKey="2122ddcfccbb3a4b1cd17bdca7b9c889"
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=berlin&units=metric" 
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=" 
 
-async function checkWeather(){
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+
+async function checkWeather(city){
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
 
-  console.log(data);
+  document.querySelector('.city').innerHTML = data.name;
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp)+"°c";
+  document.querySelector('.humidity').innerHTML = data.main.humidity+" %";
+  document.querySelector('.wind').innerHTML = data.wind.speed+" km/h";
   
 }
 
-checkWeather();
+searchBtn.addEventListener( "click" , () => {
+    checkWeather(searchBox.value)
+})
